14490f1ad39587492901e279a0689b35cc5e64d5
Morgante Pell <morgante.pell@morgante.net>
2012-07-25 02:02:03 -0400

    include preview screenshot for themes

--- project.github.php
+++ project.github.php
@@ -3,11 +3,14 @@
 
 		<?php if( $post->project->platform == 'habari' ): ?><p class="intro"><?php echo $post->title; ?> is a <?php echo $post->project->type; ?> developed for the <a href="http://habariproject.org">Habari</a> blogging platform and released under the <a href="<?php echo $post->project->license['url']; ?>" rel="license"><?php echo $post->project->license['name']; ?></a>.</span></p><?php endif; ?>
 			
-		 <?php echo $post->content_out; ?>
+		<?php echo $post->content_out; ?>
 		
-		<h1>GitHub</h1>
-		<p><?php echo $post->title; ?> is hosted on <a href="<?php echo $post->project->github; ?>">GitHub</a>. You can grab the current version (<strong><?php echo $post->project->version; ?></strong>) by cloning the GitHub <a href="<?php echo $post->project->cloneurl; ?>">repository</a>. I also encourage you to fork the project.</p>
-		<pre class="code shell">git clone <em><?php echo $post->project->cloneurl; ?></em></pre>
+		<?php if( $post->project->screenshot_url ): ?>
+		<div id="screenshots">
+			<h1>Screenshot</h1>
+			<img src="<?php echo $post->project->screenshot_url; ?>" alt="Screenshot of the Scientist theme" />
+		</div>
+		<?php endif; ?>
 		
 		<?php if( $post->project->help != NULL ): ?>
 			
@@ -58,6 +61,10 @@
 				<?php endforeach; ?>
 			</tbody>
 		</table>
+		
+		<h1>GitHub</h1>
+		<p><?php echo $post->title; ?> is hosted on <a href="<?php echo $post->project->github; ?>">GitHub</a>. You can grab the current version (<strong><?php echo $post->project->version; ?></strong>) by cloning the GitHub <a href="<?php echo $post->project->cloneurl; ?>">repository</a>. I also encourage you to fork the project.</p>
+		<pre class="code shell">git clone <em><?php echo $post->project->cloneurl; ?></em></pre>
 
 		<h1>Support</h1>
 		<p>If you have any issue or suggestion concerning this project, please <a href="<?php echo $post->project->new_issue_url; ?>">open an issue</a> on GitHub.</p>
